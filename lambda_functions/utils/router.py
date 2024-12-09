44f95090dfb9e5e2e11dd3555fabc0d1f57c0784
y.morimoto <y.morimoto@dispensia.co.jp>
2024-12-10 00:50:44 +0900

    update: クエリパラメータの追加

--- lambda_functions/utils/router.py
+++ lambda_functions/utils/router.py
@@ -48,32 +48,50 @@ class Router():
         method: str = event["httpMethod"]
         path: str = event["path"]
         headers: str = event["headers"]
-        body: dict = json.dumps(event["body"])
+        queryStringParameters: dict = event["queryStringParameters"]
+        body: dict = json.loads(event["body"])
 
         key = self.gen_func_key(method, path)
         target = self.decorated_functions[key]
         kwargs = self.get_path_params(target['path'], path)
         kwargs = {
-            'headers': headers,
-            'body': body,
+            "headers": headers,
+            "body": body,
+            "queryParams": queryStringParameters,
             **kwargs
         }
         return await target['function'](**kwargs)
 
 
 if __name__ == '__main__':
+    from asyncio import get_event_loop
+
     router = Router()
 
+    # 引数には`**kwargs`が必須です
     @router.get('/documents/{id}/{document_id}/chunk')
-    def get_documents(id: str, document_id: str):
+    async def get_documents(id: str, document_id: str, headers: dict, body: dict, **kwargs):
         print("ドキュメントの取得: id=", id)
         print("ドキュメントの取得: document_id=", document_id)
+        print("headers=", headers)
+        print("body= ", body["body_item1"])
 
     @router.post('/documents/{id}/{document_id}/chunk')
-    def update_documents(id: str, document_id: str):
+    async def update_documents(id: str, document_id: str, **kwargs):
         print("ドキュメントの更新: id=", id)
         print("ドキュメントの更新: document_id=", document_id)
 
-    payload = {"httpMethod": "GET",
-               "path": "/documents/sidf0930-s9d90/sd89sd0-sasd3a/chunk"}
-    router.handler(payload)
+    # Lambdaプロキシ統合のペイロード内容
+    event = {
+        "httpMethod": "GET",
+        "path": "/documents/sidf0930-s9d90/sd89sd0-sasd3a/chunk",
+        "headers": {
+            "x-acc-identification-name": "company1"
+        },
+        "queryStringParameters": {
+            "x-acc-identification-name": "company1"
+        },
+        "body": '{"body_item1": "item1", "body_item2": "item2"}'
+    }
+    result = get_event_loop().run_until_complete(router.handler(event))
+    print(result)
